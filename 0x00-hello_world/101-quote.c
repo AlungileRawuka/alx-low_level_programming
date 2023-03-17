7cbf74c747f755662fb2135572e26945193eb0c7
AlungileRawuka <alungilerawuka@gmail.com>
2023-03-16 18:45:23 -0700

    Correct 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-	char str[] = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
-	int len = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	char str[] = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
+	int len = strlen(str);
         
 	write(2,
 	      str,
