2c8fe35bd1040360a5a47326844dffc0b5f145fc
AlungileRawuka <alungilerawuka@gmail.com>
2023-03-16 18:34:19 -0700

    Add and modify 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ * main - prints a string in the standard error
+ * Return: 1
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+	int len = strlen(str);
+        
+	write(2,
+	      str,
+	     len);
+	return (1);
+}
