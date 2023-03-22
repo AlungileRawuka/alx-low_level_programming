cbacb7b1097ae0b35fee33334f2eac945b6cf203
AlungileRawuka <alungilerawuka@gmail.com>
2023-03-22 12:58:16 -0700

    Add 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ *main - prints the first 50 Fibonacci numbers
+ *
+ * Return: 0(Always)
+ */
+int main(void)
+{
+	int a;
+	unsigned int n1, n2, n3;
+
+	a = 1;
+	n1  = 1;
+	n2 = 2;
+
+	printf("%u, %u, ", n1, n2);
+	while (a < 49)
+	{
+		n3 = n1 + n2;
+		printf("%u", n3);
+		n1 = n2;
+		n2 = n3;
+		if (a != 48)
+			printf(", ");
+
+		a++;
+	}
+	printf("\n");
+
+	return (0);
+
+}
