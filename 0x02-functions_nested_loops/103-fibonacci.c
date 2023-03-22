e547785326614c305171a8a33a290b51ab6646e1
AlungileRawuka <alungilerawuka@gmail.com>
2023-03-22 14:19:35 -0700

    Add 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ *main - prints sum of all even fibonacci terms less than 4,000,000
+ *with fibonacci series starting with 1, 2
+ *
+ * Return: 0(Always)
+ */
+int main(void)
+{
+	unsigned long int n1, n2, n3, sum;
+
+	n1  = 1;
+	n2 = 2;
+	n3 = n1 + n2;
+	sum = 0;
+
+	while (n3 < 4000000)
+	{
+		n3 = n1 + n2;
+		n1 = n2;
+		n2 = n3;
+		if ((n3 % 2) == 0)
+			sum = sum + n3;
+	}
+	printf("%lu\n", sum);
+
+	return (0);
+
+}
