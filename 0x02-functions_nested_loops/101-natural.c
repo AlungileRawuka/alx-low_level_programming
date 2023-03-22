e6ba99b01d396e47df70e9fb1b80261791849ffa
AlungileRawuka <alungilerawuka@gmail.com>
2023-03-22 10:24:02 -0700

    Add 101-natural.c and modify main.h

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *add_multiples -computes a sum of multiples of 3 and 5  below 1024
+ *
+ */
+void add_multiples(void)
+{
+	int sum, n;
+
+	n = 0;
+	sum = 0;
+
+	while (n < 1024)
+	{
+		if ((n % 3) == 0 || (n % 5) == 0)
+			sum = sum + n;
+		n++;
+	}
+	printf("%d", sum);
+
+
+
+}
