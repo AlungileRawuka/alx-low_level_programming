4a85f29b3b4f3b9a8d538e172a6ebee310eb7f8b
AlungileRawuka <alungilerawuka@gmail.com>
2023-03-23 14:54:34 -0700

    Add 2-mul.c and modify main.h

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *mul - multiplies two integers and returns the result
+ *@a: first integer
+ *@b: second integer
+ *
+ * Return: product of two integers
+ */
+int mul(int a, int b)
+{
+	long int product;
+
+	product = a * b;
+
+	return (sum);
+
+}
