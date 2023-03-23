f655a0a4e0e857b6796a43109335c86cbc76fa99
AlungileRawuka <alungilerawuka@gmail.com>
2023-03-23 15:52:17 -0700

    Fix  4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,12 +6,12 @@
  */
 void print_most_numbers(void)
 {
-	int num = 48;
+	char num = '0';
 
-	while (num <= 57)
+	while (num <= '9')
 	{
-		if (num != 50 && num != 52)
-			_putchar(num + '0');
+		if (num != '2' && num != '4')
+			_putchar(num);
 		num++;
 	}
 	_putchar('\n');
