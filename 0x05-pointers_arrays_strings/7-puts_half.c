bf4555a9c9010904de2d512eaa88d73ea7a47efa
AlungileRawuka <alungilerawuka@gmail.com>
2023-03-27 18:38:54 -0700

    Fix 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * puts_half - prints half of a string, followed by a new line
+ * puts_half - prints second half of a string, followed by a new line
  * if length of string is odd, print the last n characters
  * where n = (length_of_string -1 / 2)
  * @str: pointer to a string
@@ -18,8 +18,7 @@ void puts_half(char *str)
 	}
 	if (length % 2 == 0)
 	{
-		length = length / 2;
-		for (a = 0; a < length; a++)
+		for (a = length / 2; a < length; a++)
 		{
 			_putchar(*(str + a));
 		}
@@ -27,8 +26,7 @@ void puts_half(char *str)
 	}
 	else
 	{
-		length = (length - 1) / 2;
-		for (b = 0; b < length; b++)
+		for (b = (length - 1) / 2; b < length; b++)
 		{
 			_putchar(*(str + b));
 		}
