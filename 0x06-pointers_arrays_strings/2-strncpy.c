1002448ff2896efce84756f1d8b06d1cf06079a8
AlungileRawuka <alungilerawuka@gmail.com>
2023-03-29 15:49:40 -0700

    Fix 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,14 +9,9 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int length_dest, length_src, i, a, b, count;
+	int length_src, a, b;
 
-	length_dest = length_src = a = i = count = 0;
-	while (*(dest + i) != '\0')
-	{
-		length_dest = length_dest + 1;
-		i++;
-	}
+	length_src = a  = 0;
 	while (*(src + a) != '\0')
 	{
 		length_src = length_src + 1;
@@ -24,7 +19,7 @@ char *_strncpy(char *dest, char *src, int n)
 	}
 	if (n >= length_src)
 	{
-		for (b = 0; b < length_dest; b++)
+		for (b = 0; b < n; b++)
 		{
 			if (b < length_src)
 				*(dest + b) = *(src + b);
@@ -36,8 +31,7 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		for (b = 0; b < n; b++)
 		{
-			*(dest + b) = *(src + count);
-			count++;
+			*(dest + b) = *(src + b);
 		}
 	}
 	return (dest);
