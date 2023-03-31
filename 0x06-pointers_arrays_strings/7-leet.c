ad807c24153648851f5cfd4659eedb7726484d30
AlungileRawuka <alungilerawuka@gmail.com>
2023-03-31 06:50:54 -0700

    Correct 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,24 +8,23 @@
 char *leet(char *s)
 {
 	int a, i;
-
+	int num[] = {52, 51, 48, 55, 49};
 	char *lower_alpha = "aeotl";
 	char *upper_alpha = "AEOTL";
-	char *num = "43071";
 
 	i = a = 0;
 	while (*(s + i) != '\n')
 	{
+		a = 0;
 		while (a < 5)
 		{
 			if (*(s + i) == *(lower_alpha + a) || *(s + i) == *(upper_alpha + a))
 			{
-				*(s + i) = *(num + a);
+				*(s + i) = num[a];
 			}
 			a++;
 		}
 		i++;
-		a = 0;
 	}
 
 	return (s);
