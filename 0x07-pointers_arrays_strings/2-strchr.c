0f87df9a02a9a313551a2dab5c118038a155abea
AlungileRawuka <alungilerawuka@gmail.com>
2023-04-03 18:23:49 -0700

    Add 2-strchr.c and modify main.h

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: character
+ * Return: string with c as the first element
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int a, is_found;
+
+	a = is_found = 0;
+
+	while (*(s + a) != '\0')
+	{
+		if (*(s + a) == c)
+		{
+			is_found = 1;
+			break;
+		}
+		a++;
+	}
+	if (is_found)
+		return (s + a);
+	else
+		return ('\0');
+
+}
