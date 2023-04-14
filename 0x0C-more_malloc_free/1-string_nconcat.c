afd7cd56389211ec340e0642b79ab0147b8eff30
AlungileRawuka <alungilerawuka@gmail.com>
2023-04-14 15:39:28 -0700

    Fix the code in  1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	ptr = malloc((sizeof(s1) + sizeof(s2) - 1));
+	ptr = malloc((sizeof(s1) + sizeof(s2)));
 
 	if (ptr == NULL)
 		return (NULL);
