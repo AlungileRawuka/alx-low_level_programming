94bcd1a2aa2a2c36fbab3693854b4e8cefd3f47f
AlungileRawuka <alungilerawuka@gmail.com>
2023-04-18 17:19:44 -0700

    Add dog.h and README.md

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines the name, age and owner of an object dog
+ * @name: string representing dog's name
+ * @age: represents a dog's age
+ * @owner: string representing the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
