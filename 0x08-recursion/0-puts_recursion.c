71513919294a750e6b9457d6f1704573e0e82473
AlungileRawuka <alungilerawuka@gmail.com>
2023-07-06 15:28:05 -0700

    Fix the code in 0-puts_recursion.c to accommodate empty strings

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,7 +7,6 @@
  */
 void _puts_recursion(char *s)
 {
-
 	if (*(s + 0) != '\0')
 	{
 		_putchar(*(s + 0));
@@ -15,7 +14,10 @@ void _puts_recursion(char *s)
 		_puts_recursion(s);
 
 		if (*(s + 0) == '\0')
+		{
+			_putchar(*(s + 0));
 			_putchar('\n');
+		}
 	}
 }
 
