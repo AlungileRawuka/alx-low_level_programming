f87e7e542a0a002094997d56cbc4499b97303509
AlungileRawuka <alungilerawuka@gmail.com>
2023-09-27 15:05:47 -0700

    fix the code in 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+ #include <stdio.h>
 #include "main.h"
 /**
  * _puts_recursion - prints a string, followed by a new line
@@ -11,13 +11,13 @@ void _puts_recursion(char *s)
 	{
 		_putchar(*(s + 0));
 		s++;
-		_puts_recursion(s);
 
 		if (*(s + 0) == '\0')
 		{
-			_putchar(*(s + 0));
 			_putchar('\n');
 		}
+
+		_puts_recursion(s);
 	}
 }
 
