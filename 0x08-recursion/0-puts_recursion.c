eb3f6ba9654912678a6597f8e72adf57e1109531
AlungileRawuka <alungilerawuka@gmail.com>
2023-09-27 15:34:54 -0700

    Remove trailing white space for betty compliance

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,4 @@
- #include <stdio.h>
+#include <stdio.h>
 #include "main.h"
 /**
  * _puts_recursion - prints a string, followed by a new line
@@ -16,7 +16,7 @@ void _puts_recursion(char *s)
 	{
 		_putchar(*(s + 0));
 		s++;
-		
+
 		_puts_recursion(s);
 	}
 }
