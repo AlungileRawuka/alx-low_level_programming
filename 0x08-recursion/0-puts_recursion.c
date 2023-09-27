4af54e0e7393261409901d7bdd1742003a0032ea
AlungileRawuka <alungilerawuka@gmail.com>
2023-09-27 15:31:02 -0700

    Fix the code in 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,16 +7,16 @@
  */
 void _puts_recursion(char *s)
 {
+	if (*(s + 0) == '\0')
+	{
+		_putchar('\n');
+	}
+
 	if (*(s + 0) != '\0')
 	{
 		_putchar(*(s + 0));
 		s++;
-
-		if (*(s + 0) == '\0')
-		{
-			_putchar('\n');
-		}
-
+		
 		_puts_recursion(s);
 	}
 }
