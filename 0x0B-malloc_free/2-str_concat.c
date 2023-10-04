8c25d7b63e41c428ca006a1cc1b6700751584011
AlungileRawuka <alungilerawuka@gmail.com>
2023-10-04 00:47:26 -0700

    Handle NULL inputs

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,17 +12,17 @@ char *str_concat(char *s1, char *s2)
 	int i, s1_len, s2_len;
 
 	s1_len = s2_len = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	while (*(s1 + s1_len) != '\0')
-	{
 		s1_len++;
-	}
 
 	while (*(s2 + s2_len) != '\0')
-	{
 		s2_len++;
-	}
-	s3 = malloc((s1_len + s2_len) - 1);
+	s3 = malloc(sizeof(char) * ((s1_len + s2_len) - 1));
 	if (s3 == NULL)
 		return (NULL);
 	for (i = 0; i < s1_len; i++)
