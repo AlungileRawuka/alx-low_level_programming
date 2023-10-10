d5b81598485a5606bc5db1afb0eba2f30f66e2cf
AlungileRawuka <alungilerawuka@gmail.com>
2023-10-10 03:37:04 -0700

    Fix dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,20 +15,7 @@ struct dog
 	float age;
 	char *owner;
 };
-/**
- * struct dog_t - struct giving a  new name for the struct dog
- * @name: string name
- * @age: float age
- * @owner: string owner name
- *
- * Description: struct giving a new name to struct dog
- */
-typedef struct dog_t
-{
-	char *name;
-	float age;
-	char *owner;
-} dog;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
