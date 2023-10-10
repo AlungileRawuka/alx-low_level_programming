9c5d254ccefe8fbc87e09fa50465f1ee0311d4e8
AlungileRawuka <alungilerawuka@gmail.com>
2023-10-10 03:15:35 -0700

    Use typedef to give struct dog a new name dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,7 +15,20 @@ struct dog
 	float age;
 	char *owner;
 };
-
+/**
+ * struct dog_t - struct giving a  new name for the struct dog
+ * @name: string name
+ * @age: float age
+ * @owner: string owner name
+ *
+ * Description: struct giving a new name to struct dog
+ */
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
